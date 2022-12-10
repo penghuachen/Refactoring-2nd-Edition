3652763945ccc44876678abd12f373a3571f67fd
penghuachen <kids5346@gmail.com>
2022-12-10 16:34:41 +0800

    refactor: remove thisAmount vairable and use inline variable instead

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -10,7 +10,6 @@ function statement (invoice, plays) {
     ).format
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
 
     // 加入 volume credit
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -19,8 +18,8 @@ function statement (invoice, plays) {
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 印出這筆訂單
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
