2f3d13a82ba00f5eb26a26abad8e73c0f2029e16
penghuachen <kids5346@gmail.com>
2022-12-10 16:28:34 +0800

    refactor: remove play variable and use inline variable instead

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -10,17 +10,16 @@ function statement (invoice, plays) {
     ).format
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // 加入 volume credit
     volumeCredits += Math.max(perf.audience - 30, 0);
 
     // 每十名喜劇觀眾可獲得額外點數
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 印出這筆訂單
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
